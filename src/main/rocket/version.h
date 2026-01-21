54adabe49cb1bb108ba4d3c7bb18cc6fc7fa2814
Philip Köster <philip.koester@web.de>
2026-01-21 21:14:07 +0100

    ...

--- src/main/rocket/version.h
+++ src/main/rocket/version.h
@@ -4,16 +4,16 @@
 
 #pragma once
 
-#define ROCKET_VERSION_INFO "0.2.0" ///< SemVer string.
+#define ROCKET_VERSION_NAME "0.2.0" ///< SemVer string.
 
-#define ROCKET_VERSION_MAJOR 0UL ///< Major.
-#define ROCKET_VERSION_MINOR 2UL ///< Minor.
-#define ROCKET_VERSION_PATCH 0UL ///< Patch.
+#define ROCKET_VERSION_MAJOR 0 ///< Major.
+#define ROCKET_VERSION_MINOR 2 ///< Minor.
+#define ROCKET_VERSION_PATCH 0 ///< Patch.
 
 /// Comparable version value.
 #define ROCKET_VERSION ( \
-    ROCKET_VERSION_MAJOR * 1'000'000UL + \
-    ROCKET_VERSION_MINOR * 1'000UL + \
+    ROCKET_VERSION_MAJOR * 1'000'000 + \
+    ROCKET_VERSION_MINOR * 1'000 + \
     ROCKET_VERSION_PATCH)
 
 // EOF
