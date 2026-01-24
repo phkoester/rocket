747afa56b387212e26f13fbdf1f98192f8710d1d
Philip Köster <philip.koester@web.de>
2026-01-24 23:44:07 +0100

    ...

--- build.cmd
+++ build.cmd
@@ -10,7 +10,5 @@ cmake --build --preset windows-release
 if %errorlevel% neq 0 exit /b %errorlevel%
 ctest --preset windows-release
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --install build --config Release --prefix install
-if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: EOF
