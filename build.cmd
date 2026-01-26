4ade44d80ba185342d0b365c09d85a63a8acfb20
Philip Köster <philip.koester@web.de>
2026-01-26 20:19:31 +0100

    ...

--- build.cmd
+++ build.cmd
@@ -16,9 +16,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build --preset windows-release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-copy %BUILD_DIR%\compile_commands.json .
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 ctest --preset windows-release
 if %errorlevel% neq 0 exit /b %errorlevel%
 
