c9234297f1fbe1a97494eba1719179028561e183
Philip Köster <philip.koester@web.de>
2026-01-27 18:21:54 +0100

    ...

--- build.cmd
+++ build.cmd
@@ -1,53 +1,75 @@
 ::
 :: build.cmd
 ::
-:: Usage: build.cmd
-::    or  build.cmd test-terminal
+:: Usage: build [configure | build | test | test-terminal]
 ::
 :: To build a specific target:
 ::
-::   > cmake --preset windows
 ::   > cmake --build --preset windows-release --target TARGET
 ::
 :: To run specific tests:
 ::
-::   > ctest --preset windows-release -R PATTERN --output-on-failure
 ::   > ctest --preset windows-release -R PATTERN -V
 ::   > build\src\test\Release\test-NAME.exe
 ::
 
 @echo off
 
-if %1 == test-terminal goto :test-terminal
+if %1 == configure call :configure
+if %1 == build call :build
+if %1 == test call :test
+if %1 == test-terminal call :test-terminal
+if [%1] == [] call :main
+
+goto :eof
 
 :: main ----------------------------------------------------------------------------------------------------
+
+call :configure
+call :build
+call :test
+
+goto :eof
+
+:: build ----------------------------------------------------------------------------------------------------
+
+:build
+
+cmake --build --preset windows-release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+goto :eof
+
+:: configure ------------------------------------------------------------------------------------------------
+
+:configure
 
 cmake --preset windows
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cmake --build --preset windows-release
-if %errorlevel% neq 0 exit /b %errorlevel%
+goto :eof
+
+:: test -----------------------------------------------------------------------------------------------------
+
+:test
 
 if exist src\test (
    ctest --preset windows-release
    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
-goto :EOF
+goto :eof
 
 :: test-terminal --------------------------------------------------------------------------------------------
 
 :test-terminal
 
 setlocal
-
 setROCKET_TEST_TERMINAL=1
-
 build\src\test\Release\test-rocket-system-terminal.exe
 build\src\test\Release\test-rocket-unicode-Character.exe
-
 endlocal
 
-goto :EOF
+goto :eof
 
 :: EOF
