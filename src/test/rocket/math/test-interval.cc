052159bd4df05096b9364a36bfc65fb68b5cc8f6
Philip Köster <philip.koester@web.de>
2026-01-30 22:22:29 +0100

    ...

--- src/test/rocket/math/test-interval.cc
+++ src/test/rocket/math/test-interval.cc
@@ -510,7 +510,7 @@ TEST(interval, RightOpenIntervalF32) {
 TEST(interval, OpenIntervalF32) {
   using type = OpenInterval<f32>;
 
-  type val = type(1_f32, 0_f32); // MSVC doesn't compile the default ctor
+  type val = type(optional<f32>(1_f32), optional<f32>(0_f32)); // MSVC
   EXPECT_TRUE(val.empty());
   EXPECT_EQ(val.cardinality(), 0);
   EXPECT_EQ(val.size(), 0_f32);
