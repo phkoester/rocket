b632e5a307afffa1befc3c0ba71ffbf4b6df0875
Philip Köster <philip.koester@web.de>
2026-01-31 17:02:48 +0100

    ...

--- make.cmd
+++ make.cmd
@@ -11,10 +11,27 @@
 ::   make test-terminal                        (for Rocket only)
 ::   make clean
 ::
+:: Parameters:
+::
+:: - BUILD_TYPE
+::     The build type: `debug` or `release`
 
 @echo off
 
 setlocal
+
+:: Configure build type -------------------------------------------------------------------------------------
+
+if not defined BUILD_TYPE set BUILD_TYPE=release
+if %BUILD_TYPE% neq debug if %BUILD_TYPE% neq release (
+ echo BUILD_TYPE: Invalid value `%BUILD_TYPE%`; expected `debug` or `release`
+ exit /b 1
+)
+
+if %BUILD_TYPE% == debug set CONFIG=Debug
+if %BUILD_TYPE% == release set CONFIG=Release
+
+:: Parse command --------------------------------------------------------------------------------------------
 
 if "%1" == "configure" (
   call :configure
@@ -58,9 +75,9 @@ goto :eof
 :build
 
 if [%1] == [] (
-  cmake --build --preset windows-release
+  cmake --build --preset windows-%BUILD_TYPE%
 ) else (
-  cmake --build --preset windows-release --target %1
+  cmake --build --preset windows-%BUILD_TYPE% --target %1
 )
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -74,13 +91,13 @@ set TEST=%1
 if not defined TEST set TEST=test
 
 if %TEST% == all (
-   ctest --preset windows-release
+   ctest --preset windows-%BUILD_TYPE%
 ) else if %TEST% == bench (
-   ctest --test-dir build\src\bench --preset windows-release -V
+   ctest --test-dir build\src\bench --preset windows-%BUILD_TYPE% -V
 ) else if %TEST% == test (
-   ctest --test-dir build\src\test --preset windows-release
+   ctest --test-dir build\src\test --preset windows-%BUILD_TYPE%
 ) else (
-   ctest --preset windows-release -R %TEST% -V
+   ctest --preset windows-%BUILD_TYPE% -R %TEST% -V
 )
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -92,8 +109,8 @@ goto :eof
 
 set ROCKET_TEST_TERMINAL=1
 
-build\src\test\Release\test-rocket-system-terminal.exe
-build\src\test\Release\test-rocket-unicode-Character.exe
+build\src\test\%CONFIG%\test-rocket-system-terminal.exe
+build\src\test\%CONFIG%\test-rocket-unicode-Character.exe
 
 goto :eof
 
