85545e92adc828e80fb4a69a1c4aa6ea4f597763
Philip Köster <philip.koester@web.de>
2026-02-02 11:37:02 +0100

    ...

--- src/bench/rocket/bench-map.cc
+++ src/bench/rocket/bench-map.cc
@@ -17,17 +17,19 @@
 constexpr array<const char*, 7> KEYS = { "alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf" };
 constexpr array<i32, 7> VALS = { 0, 1, 2, 3, 4, 5, 6 };
 
+// Variables ------------------------------------------------------------------------------------------------
+
+auto gen = math::gen();
+
 // #BENCH ---------------------------------------------------------------------------------------------------
 
 void
-map_map(benchmark::State& state) {
+map_smallConstMap(benchmark::State& state) {
   map<string_view, i32> map;
   for (u64 i = 0; i < KEYS.size(); ++i) {
     map.emplace(KEYS[i], VALS[i]);
   }
 
-  auto gen = math::gen();
-
   for (auto _ : state) { // NOLINT
     const u64 index = math::random(gen, 0_u64, KEYS.size() - 1);
     const auto& key = KEYS[index];
@@ -35,17 +37,15 @@ map_map(benchmark::State& state) {
   }
 }
 
-BENCHMARK(map_map);
+BENCHMARK(map_smallConstMap);
 
 void
-map_unorderedMap(benchmark::State& state) {
+map_smallConstUnorderedMap(benchmark::State& state) {
   unordered_map<string_view, i32> map;
   for (u64 i = 0; i < KEYS.size(); ++i) {
     map.emplace(KEYS[i], VALS[i]);
   }
 
-  auto gen = math::gen();
-
   for (auto _ : state) { // NOLINT
     const u64 index = math::random(gen, 0_u64, KEYS.size() - 1);
     const auto& key = KEYS[index];
@@ -53,17 +53,15 @@ map_unorderedMap(benchmark::State& state) {
   }
 }
 
-BENCHMARK(map_unorderedMap);
+BENCHMARK(map_smallConstUnorderedMap);
 
 void
-map_bimap(benchmark::State& state) {
+map_smallConstBimap(benchmark::State& state) {
   rocket::Bimap<string_view, i32> map;
   for (u64 i = 0; i < KEYS.size(); ++i) {
     map.insert({ KEYS[i], VALS[i] });
   }
 
-  auto gen = math::gen();
-
   for (auto _ : state) { // NOLINT
     const u64 index = math::random(gen, 0_u64, KEYS.size() - 1);
     const auto& key = KEYS[index];
@@ -71,17 +69,15 @@ map_bimap(benchmark::State& state) {
   }
 }
 
-BENCHMARK(map_bimap);
+BENCHMARK(map_smallConstBimap);
 
 void
-map_unorderedBimap(benchmark::State& state) {
+map_smallConstUnorderedBimap(benchmark::State& state) {
   rocket::UnorderedBimap<string_view, i32> map;
   for (u64 i = 0; i < KEYS.size(); ++i) {
     map.insert({ KEYS[i], VALS[i] });
   }
 
-  auto gen = math::gen();
-
   for (auto _ : state) { // NOLINT
     const u64 index = math::random(gen, 0_u64, KEYS.size() - 1);
     const auto& key = KEYS[index];
@@ -89,6 +85,6 @@ map_unorderedBimap(benchmark::State& state) {
   }
 }
 
-BENCHMARK(map_unorderedBimap);
+BENCHMARK(map_smallConstUnorderedBimap);
 
 // EOF
